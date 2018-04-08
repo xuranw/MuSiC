bc1822a6c2310ebf7dbde69ab5026771150a5160
xuranw <xuranw@sas.upenn.edu>
2018-04-08 15:22:14 -0400

    Create utils.R

---
+++ R/utils.R
@@ -0,0 +1,291 @@
+# Utility functions
+#
+# Author: Xuran Wang
+# Copyright Xuran Wang (2018)
+##########################################################################
+
+
+#' Calculate relative abudance
+#' @param X non-negative matrix for calculate relative abundance
+#' @param by.col logical, default as TRUE
+#' @export
+relative.ab = function(X, by.col = TRUE){
+  if(sum(X < 0) > 0){
+    message('Negative entry appears!')
+    break;
+  }
+  if(by.col == T){
+    RX = sweep(X, 2, colSums(X), '/')
+  }else{
+    RX = sweep(X, 1, rowSums(X), '/')
+  }
+  return(RX)
+}
+
+#' Calculate square root of all values
+#' @return  sqrt(x)   x >= 0
+#'         -sqrt(-x)  x < 0
+#' @param x real number
+#' @export
+my.sqrt = function(x){
+  sign(x)*sqrt(abs(x))
+}
+
+#' Calculate fpkm to tpm
+fpkmToTpm <- function(fpkm){
+  apply(fpkm, 2, function(x){
+    exp(log(x) - log(sum(x)) + log(1e6))
+  })
+}
+
+#' log transformation with regulation
+#'
+#' @param x numeric
+#' @param nu regulation
+#'
+#' @export
+my.log = function(x, nu){
+  sign(x)*( log(abs(x) + nu)- log(nu) )
+}
+
+#' Calcuate Row Means with NA remove
+#'
+#' @param x matrix
+#' @param na.rm default FALSE
+#'
+#' @return vector of rowmeans
+#'
+#' @export
+my.rowMeans = function (x, na.rm = FALSE, dims = 1L){
+  if (is.data.frame(x))
+    x <- as.matrix(x)
+  if(length(dn <- dim(x)) < 2L){
+    return(x)
+  }
+  if (!is.array(x) || length(dn <- dim(x)) < 2L )
+    stop("'x' must be an array of at least two dimensions")
+  if (dims < 1L || dims > length(dn) - 1L)
+    stop("invalid 'dims'")
+  p <- prod(dn[-(id <- seq_len(dims))])
+  dn <- dn[id]
+  z <- if (is.complex(x))
+    .Internal(rowMeans(Re(x), prod(dn), p, na.rm)) + (0+1i) *
+    .Internal(rowMeans(Im(x), prod(dn), p, na.rm))
+  else .Internal(rowMeans(x, prod(dn), p, na.rm))
+  if (length(dn) > 1L) {
+    dim(z) <- dn
+    dimnames(z) <- dimnames(x)[id]
+  }
+  else names(z) <- dimnames(x)[[1L]]
+  z
+}
+
+#' Get upper triangle matrix
+#'
+#' @param cormat square matrix
+#' @return upper triangle matrix
+#'
+#' @export
+get_upper_tri = function(cormat){
+  cormat[lower.tri(cormat)]<- NA
+  return(cormat)
+}
+
+
+
+
+# This function is to calculate the deconvolution proportion
+#' @param bulk.eset ExpressionSet for bulk data
+#' @param sc.eset ExpressionSet for single cell data
+#' @param markers vector or list of gene names, default as NULL. If NULL, use all genes that provided by both bulk and single cell dataset.
+#' @param clusters character, the phenoData of single cell dataset used as clusters;
+#' @param samples character,the phenoData of single cell dataset used as samples;
+#' @param select.ct vector of cell types, default as NULL. If NULL, then use all cell types provided by single cell dataset;
+#' @param ct.cov logical. If TRUE, use the covariance across cell types;
+#' @param verbose logical, default as TRUE.
+#' @param iter.max numeric, maximum iteration number
+#' @param nu regulation parameter, take care of weight when taking recipical
+#' @param eps Thredshold of convergence
+#' @param inter.as.bseq logic, substract avg of Y and D as BSEQ-sc did
+#' @param normalize logic, divide Y and D by their standard deviation
+#' @return Estimation from W-NNLS, Estimation from NNLS, Weight of W-NNLS, r.squared, Var of W-NNLS
+w_nnls_prop = function(bulk.eset, sc.eset, markers = NULL, clusters, samples, select.ct = NULL, ct.cov = FALSE, verbose = TRUE,
+                       iter.max = 1000, nu = 0.0001, eps = 0.01, inter.as.bseq = F, normalize = F, ... ){
+  bulk.gene = rownames(bulk.eset)[rowMeans(exprs(bulk.eset)) != 0]
+  bulk.eset = bulk.eset[bulk.gene, , drop = FALSE]
+  if(is.null(markers)){
+    sc.markers = bulk.gene
+  }else{
+    sc.markers = intersect(bulk.gene, unlist(markers))
+  }
+  sc.basis = w_nnls_basis(sc.eset, non.zero = TRUE, markers = sc.markers, clusters = clusters, samples = samples, select.ct = select.ct, ct.cov = ct.cov, verbose = verbose)
+  cm.gene = intersect( rownames(sc.basis$Disgn.mtx), bulk.gene )
+  if(is.null(markers)){
+    if(length(cm.gene)< 0.2*min(length(bulk.gene), nrow(sc.eset)) )
+      stop("Too few common genes!")
+  }else{
+    if(length(cm.gene)< 0.2*length(unlist(markers)))
+      stop("Too few common genes!")
+  }
+  if(verbose){message(paste('Used', length(cm.gene), 'common genes...'))}
+
+  m.sc = match(cm.gene, rownames(sc.basis$Disgn.mtx)); m.bulk = match(cm.gene, bulk.gene)
+  D1 = sc.basis$Disgn.mtx[m.sc, ]; M.S = colMeans(sc.basis$S, na.rm = T);
+  Yjg = relative.ab(exprs(bulk.eset)[m.bulk, ]); N.bulk = ncol(bulk.eset);
+  if(ct.cov){
+    Sigma.ct = sc.basis$Sigma.ct[, m.sc];
+
+    if(sum(Yjg[, i] == 0) > 0){
+      D1.temp = D1[Yjg[, i]!=0, ];
+      Yjg.temp = Yjg[Yjg[, i]!=0, i];
+      Sigma.ct.temp = Sigma.ct[, Yjg[,i]!=0];
+      if(verbose) message(paste(colnames(Yjg)[i], 'has common genes', sum(Yjg[, i] != 0), '...') )
+    }else{
+      D1.temp = D1;
+      Yjg.temp = Yjg[, i];
+      Sigma.ct.temp = Sigma.ct;
+      if(verbose) message(paste(colnames(Yjg)[i], 'has common genes', sum(Yjg[, i] != 0), '...'))
+    }
+
+    lm.D1.weighted = nnls.weight.new.iter.ct(Yjg.temp, D1.temp, M.S, Sigma.ct.temp, iter.max = iter.max,
+                                             nu = nu, eps = eps, inter.as.bseq = inter.as.bseq, normalize = normalize)
+    Est.prop.allgene = rbind(Est.prop.allgene, lm.D1.weighted$p.nnls)
+    Est.prop.weighted = rbind(Est.prop.weighted, lm.D1.weighted$p.weight)
+    weight.gene.temp = rep(NA, nrow(Yjg)); weight.gene.temp[Yjg[,i]!=0] = lm.D1.weighted$weight.gene;
+    Weight.gene = cbind(Weight.gene, weight.gene.temp)
+    r.squared.full = c(r.squared.full, lm.D1.weighted$R.squared)
+    Var.prop = rbind(Var.prop, lm.D1.weighted$var.p)
+  }else{
+    Sigma = sc.basis$Sigma[m.sc, ];
+
+    valid.ct = (colSums(is.na(Sigma)) == 0)&(colSums(is.na(D1)) == 0)&(!is.na(M.S))
+
+    if(sum(valid.ct)<=1){
+      stop("Not enough valid cell type!")
+    }
+
+    if(verbose){message(paste('Used', sum(valid.ct), 'cell types in deconvolution...' ))}
+
+    D1 = D1[, valid.ct]; M.S = M.S[valid.ct]; Sigma = Sigma[, valid.ct];
+
+    Est.prop.allgene = NULL
+    Est.prop.weighted = NULL
+    Weight.gene = NULL
+    r.squared.full = NULL
+    Var.prop = NULL
+    for(i in 1:N.bulk){
+      if(sum(Yjg[, i] == 0) > 0){
+        D1.temp = D1[Yjg[, i]!=0, ];
+        Yjg.temp = Yjg[Yjg[, i]!=0, i];
+        Sigma.temp = Sigma[Yjg[,i]!=0, ];
+        if(verbose) message(paste(colnames(Yjg)[i], 'has common genes', sum(Yjg[, i] != 0), '...') )
+      }else{
+        D1.temp = D1;
+        Yjg.temp = Yjg[, i];
+        Sigma.temp = Sigma;
+        if(verbose) message(paste(colnames(Yjg)[i], 'has common genes', sum(Yjg[, i] != 0), '...'))
+      }
+
+      lm.D1.weighted = nnls.weight.new.iter(Yjg.temp, D1.temp, M.S, Sigma.temp, iter.max = iter.max,
+                                            nu = nu, eps = eps, inter.as.bseq = inter.as.bseq, normalize = normalize)
+      Est.prop.allgene = rbind(Est.prop.allgene, lm.D1.weighted$p.nnls)
+      Est.prop.weighted = rbind(Est.prop.weighted, lm.D1.weighted$p.weight)
+      weight.gene.temp = rep(NA, nrow(Yjg)); weight.gene.temp[Yjg[,i]!=0] = lm.D1.weighted$weight.gene;
+      Weight.gene = cbind(Weight.gene, weight.gene.temp)
+      r.squared.full = c(r.squared.full, lm.D1.weighted$R.squared)
+      Var.prop = rbind(Var.prop, lm.D1.weighted$var.p)
+    }
+  }
+  colnames(Est.prop.weighted) = colnames(D1)
+  rownames(Est.prop.weighted) = colnames(Yjg)
+  colnames(Est.prop.allgene) = colnames(D1)
+  rownames(Est.prop.allgene) = colnames(Yjg)
+  names(r.squared.full) = colnames(Yjg)
+  colnames(Weight.gene) = colnames(Yjg)
+  rownames(Weight.gene) = cm.gene
+  colnames(Var.prop) = colnames(D1)
+  rownames(Var.prop) = colnames(Yjg)
+
+  return(list(Est.prop.weighted = Est.prop.weighted, Est.prop.allgene = Est.prop.allgene,
+              Weight.gene = Weight.gene, r.squared.full = r.squared.full, Var.prop = Var.prop))
+}
+
+
+
+
+
+######## Transform ExpressionSet to Seurat Class ##############
+EsettoSeurat = function(eset){
+  nbt=new("seurat",raw.data=exprs(eset))
+  nbt@cell.names = rownames(pData(eset))
+  nbt@ident = factor(eset$cellType, levels = intersect(levels(eset$cellType), unique(as.character(eset$cellType))) );
+  names(nbt@ident) = colnames(eset)
+  nbt@meta.data = data.frame(nGene = colSums(exprs(eset) != 0 ), sampleID = eset$sampleID, SubjectName = eset$SubjectName)
+  if('Disease'%in%colnames(pData(eset))){
+    nbt@meta.data$Disease = eset$Disease
+  }
+  return(nbt)
+}
+
+
+
+################### ANOVA select Marker Genes ######################
+Anova_info = function(eset, non.zero = T, markers = NULL, clusters, samples, select.ct = NULL, num.info = 25, ... ){
+  if(!is.null(select.ct)){
+    s.ct = sampleNames(eset)[as.character(pVar(eset, clusters)) %in% select.ct]
+    eset <- eset[, s.ct, drop = FALSE]
+    message(paste('Selected', length(select.ct), 'cell type(s) ...' ))
+  }
+  if(non.zero){  ## eliminate non expressed genes
+    nz.gene = rownames(eset)[( rowSums(exprs(eset)) != 0 )]
+    eset <- eset[nz.gene, , drop = FALSE]
+    message(paste('Eliminating non expressed gene(s) ...' ))
+  }
+  if(!is.null(markers)){
+    eset <- eset[unlist(markers), , drop = FALSE]
+    message(paste('Selected', length(unlist(markers)), 'marker gene(s) ...' ))
+  }
+
+  exprs(eset) = log( relative.ab(exprs(eset))+10^{-10} ) #change counts to relative abundance
+  message('Transform counts to log scaled relative abundance ...')
+  clusters <- as.character(pVar(eset, clusters))
+  samples <- as.character(pVar(eset, samples))
+  cell.type.select = unique(as.character(pVar(eset, clusters)))
+  ngene = nrow(eset);
+  j = 1
+  #message(paste0('Selected ', length(cell.type.select), ' cell type(s) ...'))
+  F.indv = NULL; F.cell.type = NULL; F.inter = NULL;
+  for(i in 1:nrow(eset)){
+    f.indv = NULL; f.cell.type = NULL; f.inter = NULL;
+    marker.data = data.frame(log.RA = exprs(eset)[i, ], indv = samples, cell.type = clusters)
+    s.aov = unlist(summary(aov(log.RA ~ indv*cell.type, data = marker.data)))[13:15]
+    f.indv = c(f.indv, s.aov[1]); f.cell.type = c(f.cell.type, s.aov[2]); f.inter = c(f.inter, s.aov[3])
+
+    for(k in 1:length(cell.type.select)){
+      marker.data.1ct = marker.data;
+      cell.type.1ct = rep(cell.type.select[k], ncol(eset));
+      cell.type.1ct[(marker.data$cell.type != cell.type.select[k])] = paste('non', cell.type.select[k]);
+      marker.data.1ct$cell.type = cell.type.1ct;
+      ct.aov = unlist(summary(aov(log.RA ~ indv*cell.type, data = marker.data.1ct)) )[13:15];
+      f.indv = c(f.indv, ct.aov[1]); f.cell.type = c(f.cell.type, ct.aov[2]); f.inter = c(f.inter, ct.aov[3]);
+    }
+    F.indv = rbind(F.indv, f.indv); F.cell.type = rbind(F.cell.type, f.cell.type);
+    F.inter = rbind(F.inter, f.inter);
+    if(ngene >=  10000){
+      if(i == round(ngene*j*0.1)){
+        message(paste(j*10, '% is done ...'));
+        j = j+ 1;
+      }
+    }
+  }
+  #cat(dim(F.indv), dim(F.cell.type), dim(F.inter))
+  rownames(F.indv) <- rownames(F.cell.type) <- rownames(F.inter) <- rownames(eset);
+  colnames(F.indv) <- colnames(F.cell.type) <- colnames(F.inter) <- c('full', cell.type.select)
+  marker.score = log(F.cell.type/F.indv)
+  info.gene = NULL; num.info = min(num.info, nrow(F.indv));
+  for(k in 1:ncol(F.indv)){
+    info.gene = cbind(info.gene, rownames(marker.score)[order(marker.score[, k], decreasing = T)[1:num.info]])
+  }
+  colnames(info.gene) = colnames(marker.score)
+  return( list(F.indv = F.indv, F.cell.type = F.cell.type, F.inter = F.inter, informative.gene = info.gene) )
+}

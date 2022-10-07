97f94b286c234fb566b28bb366253f18d2c5f39d
Xuran Wang <29867659+xuranw@users.noreply.github.com>
2022-10-06 22:04:03 -0400

    Add files via upload

---
+++ R/zzz.R
@@ -0,0 +1,4 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("MuSiC v1.0.0 support SingleCellExperiment! See Tutorial: https://xuranw.github.io/MuSiC/articles/MuSiC.html")
+  packageStartupMessage("MuSiC2 for multi-condition bulk RNA-seq data is also available!")
+}
